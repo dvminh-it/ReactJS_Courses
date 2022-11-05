ac797b620051ec1619f1ca6a969d3bb9a5d9b1cd
dvminh-it <dvminh.it@gmail.com>
2022-11-05 19:09:39 +0700

    Removed folder from repository

--- crud-hook/app/src/saga/index.js
+++
@@ -1,9 +0,0 @@
-import { all } from 'redux-saga/effects';
-import studentSaga from "./studentSaga";
-
-function* rootSaga() {
-    yield all([
-        studentSaga()
-    ]);
-}
-export default rootSaga;
